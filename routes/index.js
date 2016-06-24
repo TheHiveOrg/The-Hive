11236e2239ebfabb846d4ec0796cd7b3013284bc
Heather Stone <heather.blair.stone@gmail.com>
2016-06-24 13:43:07 -0600

    add blank handlebars pages and auth, knexjs, and api

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/api');
+var auth = require('../auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
