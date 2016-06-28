8ff2ca90bc1664aa19532f030704981661c232f0
Heather Stone <heather.blair.stone@gmail.com>
2016-06-28 11:33:16 -0600

    add route for about us and change titles of pages

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,12 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/aboutUs', function(req, res, next) {
+    res.render('aboutUs', {
+        title: 'About Us'
+    });
+});
+
 router.get('/auth/google', auth.passport.authenticate('google', {
     scope: ['openid email profile']
 }));
@@ -28,43 +34,43 @@ router.get('/signOut', function(request, response, next) {
 
 router.get('/userProfile', function(req, res, next) {
     res.render('userProfile', {
-        title: 'Express'
+        title: 'User Profile'
     });
 });
 
 router.get('/editProfile', function(req, res, next) {
     res.render('editProfile', {
-        title: 'Express'
+        title: 'Edit Profile'
     });
 });
 
 router.get('/addBee', function(req, res, next) {
     res.render('addBee', {
-        title: 'Express'
+        title: 'Add Bee'
     });
 });
 
 router.get('/beeInfo', function(req, res, next) {
     res.render('beeInfo', {
-        title: 'Express'
+        title: 'Bee Info'
     });
 });
 
 router.get('/beeMap', function(req, res, next) {
     res.render('beeMap', {
-        title: 'Express'
+        title: 'Bee Map'
     });
 });
 
 router.post('/editProfile', function(req, res, next) {
     res.render('editProfile', {
-        title: 'Express'
+        title: 'Edit Profile'
     });
 });
 
 router.post('/addBee', function(req, res, next) {
     res.render('addBee', {
-        title: 'Express'
+        title: 'Add Bee'
     });
 });
 
