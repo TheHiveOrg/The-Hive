b335e41d66e292d33e7c8209f699561085fca2d9
Heather Stone <heather.blair.stone@gmail.com>
2016-06-29 14:35:02 -0600

    adding database info to add bee

--- db/api.js
+++ db/api.js
@@ -13,14 +13,6 @@ module.exports = {
       last_name: profileId.name.familyName,
       username: profileId.emails[0].value,
       avatar: profileId.photos[0].value,
-      google_id: profileId.id
-    })
-  }
-
-    return knex('user').insert({first_name: profileId.name.givenName,
-                                last_name: profileId.name.familyName,
-                                username: profileId.emails[0].value,
-                                avatar: profileId.photos[0].value,
-                                google_id: profileId.id})
+      google_id: profileId.id})
     }
 };
