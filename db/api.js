77ce4161db232d5aae52b864832592e2aebd2151
Heather Stone <heather.blair.stone@gmail.com>
2016-06-29 15:01:43 -0600

    TAKE THIS

--- db/api.js
+++ db/api.js
@@ -1,19 +1,21 @@
+
 var knex = require('./knex');
 module.exports = {
     findUserById: function(profileId) {
-        return knex('user').select().where("google_id", profileId).first().then(function(data) {
+        return knex('users').select().where("google_id", profileId).first().then(function(data) {
             if (data) {
                 return data.google_id;
             }
         });
     },
     createUser: function(profileId) {
-        return knex('user').insert({
+        return knex('users').insert({
             first_name: profileId.name.givenName,
             last_name: profileId.name.familyName,
             username: profileId.emails[0].value,
             avatar: profileId.photos[0].value,
             google_id: profileId.id
         });
+
     }
 };
