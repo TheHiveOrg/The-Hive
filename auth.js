3b240c4a5d74aed31ace655028e2d420ff728dea
Heather Stone <heather.blair.stone@gmail.com>
2016-06-29 15:06:29 -0600

    fix callback url to heroku url

--- auth.js
+++ auth.js
@@ -11,7 +11,8 @@ passport.deserializeUser(function(obj, done){
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  callbackURL: 'http://the-hive-g25.herokuapp.com/beeseed'
+  // 'http://localhost:3000/auth/google/callback'
   },
 
 function(accessToken, refreshToken, profile, done){
