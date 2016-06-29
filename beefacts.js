e86c62d9b2db8c96a0f6b02088b3ad7470f54778
Heather Stone <heather.blair.stone@gmail.com>
2016-06-28 20:21:47 -0600

    start bee fact api

---
+++ beefacts.js
@@ -0,0 +1,9 @@
+module.exports = [
+{
+image:
+heading:
+fact: "Bumblebees (genus Bombus) have an unusual way of incubating their eggs. Queens create a little straw nest for their eggs and sit on them to keep them warm, much like birds do. She continues to keep her young warm until long after the larvae have hatched--when they are ready to leave the nest to forage by themselves. To keep herself comfortable sitting for so long,
+
+In the spring, a new queen bumble bee incubates her eggs in a little nest of straw much like a mother bird. By placing her abdomen over the eggs she is able to control their temperature, speeding up the development of her young. Once the eggs have hatched and the larvae have emerged, she will continue to keep her daughters warm until they are old enough to leave the nest for foraging. In order to retain her sitting position eggs for as long as possible she first constructs a little wax pot filled with sweet nectar next to the nest that she can sip from so that doesn’t have to leave her young too often."
+}
+]
