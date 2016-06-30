66483acc149eaa33479db2d9c9b1bfec35061973
Heather Stone <heather.blair.stone@gmail.com>
2016-06-30 14:43:24 -0600

    fix callback url

--- auth.js
+++ auth.js
@@ -12,7 +12,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  callbackURL: 'http://the-hive-g25.herokuapp.com/auth/google/callback'
 
   },
 
--- public/javascript/beeMap.js
+++ public/javascript/beeMap.js
@@ -31,7 +31,7 @@ for(var i = 0; i < beeData.length; i++){
 function initMap() {
 
 
-  $.get("http://localhost:3000/beeseed", function(data) {
+  $.get("http://the-hive-g25.herokuapp.com/beeseed", function(data) {
     addPins(data)
   });
 
@@ -83,9 +83,9 @@ function handleNoGeolocation(errorFlag) {
 $(document).ready(function() {
 $("input[type='button']").click(function() {
     switch(this.id) {
-      case 'all': var Data = $.get("http://localhost:3000/beeseed");
+      case 'all': var Data = $.get("http://the-hive-g25.herokuapp.com/beeseed");
       break;
-      case 'user': var Data = $.get("http://localhost:3000/mapData");
+      case 'user': var Data = $.get("http://the-hive-g25.herokuapp.com/mapData");
       break;
     }
     Data.then(function(data) {
