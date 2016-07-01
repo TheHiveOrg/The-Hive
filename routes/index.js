dab4bcd52a5e9849165ff9d011f6ed2491ff8339
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-06-30 18:37:32 -0600

    fix feed

--- routes/index.js
+++ routes/index.js
@@ -66,7 +66,7 @@ router.get('/userProfile', ensureAuthenticated, function(req, res, next) {
         knex('bee_info').select('bee_info.id as bee_info_id', '*').join('users', 'user_id', '=', 'users.id').where('google_id', req.user.id)
     ]).then(function(data) {
 
-        // console.log(data[1]);
+        console.log(data);
         res.render('userProfile', {
             username: data[0][0],
             user: req.user,
